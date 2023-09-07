a3a4d0059764fd480ad5bd8e252afbbdf5b4181a
Elvinas Piliponis <elvinas.piliponis@centric.eu>
2023-09-07 13:53:21 +0300

    Another script modification

--- main-script.ps1
+++ main-script.ps1
@@ -1,2 +1,4 @@
 Write-Host "Example of a working script"
 
+Write-Host "Printing finish line"
+Write-Host "---------------------------------------------------"
--- main-script.sh
+++ main-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "This is an example of working code"
-echo "Adding additional command"
 
 uname -a
 hostname -f
 
+echo "Additional command moved down"
 
 exit 0
