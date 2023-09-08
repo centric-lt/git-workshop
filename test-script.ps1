d97706a39f8e7b83f4d99f0613181c82adf06e56
Elvinas Piliponis <elvinas.piliponis@centric.eu>
2023-09-08 10:56:35 +0300

    Test scripts updated

--- test-script.ps1
+++ test-script.ps1
@@ -1,10 +1,24 @@
+$BRez=-1
+$Rez=0
 
-Powershell.exe -executionpolicy Unrestricted -File  ".\main-script.ps1"
-$Rez=$LASTEXITCODE
+while ( $BRez -ne 0 ) {
 
+    Powershell.exe -executionpolicy Unrestricted -File  ".\main-script.ps1"
+    $Rez=$LASTEXITCODE
 
-if ( $Rez -eq 0 ) {
-    git bisect good
-} else {
-    git bisect bad
+    if ( $Rez -eq 0 ) {
+        Wtite-Host "********* GOOD Branch **********"
+        git status | Select-String -Pattern " at " -CaseSensitive -SimpleMatch
+        git bisect good | Select-String -Pattern '0 revisions left to test after this' -CaseSensitive -SimpleMatch -Quiet
+        $BRez=$LASTEXITCODE
+
+    } else {
+        Wtite-Host "********** BAD Branch **********"
+        git status | Select-String -Pattern " at " -CaseSensitive -SimpleMatch
+        git bisect bad | Select-String -Pattern '0 revisions left to test after this' -CaseSensitive -SimpleMatch -Quiet
+        $BRez=$LASTEXITCODE
+
+    }
 }
+
+git status
--- test-script.sh
+++ test-script.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
 REZ=0
-bash ./main-script.sh
-REZ=$?
+BREZ=-1
 
-if [ "$REZ" = "0" ]; then
-    git bisect good
-else
-    git bisect bad
-fi
+while [ $BREZ -ne 0 ]; do
+
+    bash ./main-script.sh > /dev/null 2>&1
+    REZ=$?
+
+    if [ "$REZ" = "0" ]; then
+        echo "********* GOOD Branch **********"
+        git status | grep " at "
+        git bisect good | grep -q '0 revisions left to test after this'
+        BREZ=$?
+    else
+        echo "********** BAD Branch **********"
+        git status | grep " at "
+        git bisect bad | grep -q '0 revisions left to test after this'
+        BREZ=$?
+    fi
+
+done
+
+git status
